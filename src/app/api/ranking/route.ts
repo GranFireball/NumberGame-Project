57ff44cf6e47cb7513eafa6c1ea687b90cb9c97d
GranFireball <leonardohk10@hotmail.com>
2024-05-24 15:37:54 -0300

    feat: force dinamic ranking route

--- src/app/api/ranking/route.ts
+++ src/app/api/ranking/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import connectMongoDB from "@/lib/mongodb";
 import RankingPlayerModel from "@/models/RankingPlayer";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   await connectMongoDB();
   const ranking = await RankingPlayerModel.find();
